40f4ca56a5dce0826c411de301da2507872f285c
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2024-07-13 15:52:12 -0400

    remove route

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -41,7 +41,7 @@
         tabindex="0"
         class="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-bold"
       >
-        <li><a href="/upgrade">Pricing</a></li>
+        <li><a href="/pricing">Pricing</a></li>
         <li><a href="/account">Account</a></li>
         <li>
           <a
